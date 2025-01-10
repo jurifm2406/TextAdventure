78a33200e0070083214fc7ef6e11df85f3ae1d0e
Franz Münzner <jurifm@icloud.com>
2025-01-10 11:38:51 +0100

    made Entity class abstract and introduced inheriting Entity classes

--- src/model/objects/base/entities/Entity.kt
+++ src/model/objects/base/entities/Entity.kt
@@ -1,15 +1,16 @@
-package model.objects.base
+package model.objects.base.entities
 
+import model.objects.base.Inventory
 import model.objects.base.item.Armor
 import model.objects.base.item.Weapon
 import kotlin.math.floor
 
-class Entity(
+abstract class Entity(
     val name: String,
     var health: Int = 20,
     val inventory: Inventory = Inventory(8),
-    val weapon: Weapon = Weapon("Fists", "Mighty fists", 2),
-    val armor: Armor = Armor("Nothing", "Bare skin", 0, 1.0)
+    val weapon: Weapon = Weapon("fists", "mighty fists", 2),
+    val armor: Armor = Armor("nothing", "bare skin", 0, 1.0)
 ) {
     fun attack(target: Entity) {
         target.health -= floor((weapon.damage - target.armor.absorbtion) * target.armor.negation) as Int
---
+++ src/model/objects/base/entities/NPC.kt
@@ -0,0 +1,9 @@
+package model.objects.base.entities
+
+import model.objects.base.Inventory
+import model.objects.base.item.Armor
+import model.objects.base.item.Weapon
+
+class NPC(name: String, health: Int, inventory: Inventory, weapon: Weapon, armor: Armor) :
+    Entity(name, health, inventory, weapon, armor) {
+}
