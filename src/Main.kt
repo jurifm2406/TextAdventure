ed9ef7b4cc1e29ec0438a5adfe9309053b01bc1a
Franz Münzner <jurifm@icloud.com>
2025-01-15 19:37:33 +0100

    added properties for macos menu bar

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,9 @@ import controller.Controller
 import javax.swing.SwingUtilities
 
 fun main() {
+    System.setProperty("apple.laf.useScreenMenuBar", "true")
+    System.setProperty("com.apple.mrj.application.apple.menu.about.name", "TextAdventure")
+
     SwingUtilities.invokeLater {
         val controller = Controller()
     }
