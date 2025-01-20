fbc783272e732ae28df8051e82ca85c287fb1e01
Franz Münzner <jurifm@icloud.com>
2025-01-20 09:18:34 +0100

    changed Directions to match natural directions

--- src/model/objects/world/Directions.kt
+++ src/model/objects/world/Directions.kt
@@ -1,8 +1,8 @@
 package model.objects.world
 
 object Directions {
-    const val NORTH = 0
-    const val EAST = 1
-    const val SOUTH = 2
-    const val WEST = 3
+    const val NORTH = 3
+    const val EAST = 2
+    const val SOUTH = 1
+    const val WEST = 0
 }
