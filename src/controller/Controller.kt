d9643404197e0bd59411f2b53a9b6687bb292c0c
Franz Münzner <jurifm@icloud.com>
2025-01-23 10:06:04 +0100

    changed command handling

--- src/controller/Controller.kt
+++ src/controller/Controller.kt
@@ -4,6 +4,8 @@ import model.Model
 import model.objects.base.item.Armor
 import model.objects.base.item.Consumable
 import model.objects.base.item.Weapon
+import model.objects.world.Directions
+import model.objects.world.ItemNotThereException
 import model.objects.base.entities.Enemy
 import model.objects.base.entities.Entity
 import model.objects.base.entities.Hero
@@ -38,7 +40,7 @@ class Controller {
         ),
         "inventory" to mapOf(
             "drop" to null,
-            "take" to null,
+            "pickup" to null,
             "info" to null
         ),
         "help" to mapOf(
@@ -138,35 +140,70 @@ class Controller {
     fun parseInput(input: String) {
         val splitInput = input.split(" ")
 
-        if (splitInput[0] in Commands.root) {
-            if (splitInput[0] == "move") {
-                if (splitInput[1] in Commands.movement) {
-                    try {
-                        model.map.move(Commands.movement.indexOf(splitInput[1]), model.hero)
-                        updateMap()
-                        view.content.output.respond("moved to the ${splitInput[1]}.")
-
-                        if(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.filterIsInstance<Enemy>().isNotEmpty()){
-                            for (i in 0 ..< model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.size){
-                                view.content.output.respond(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities[i].name)
-                            }
-                            combat(model.hero, model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.filterIsInstance<Enemy>()[0])
-                        }
-                        for (i in 0 ..< model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.inventory.content.size){
-                            view.content.output.respond(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.inventory.content[i].name)
-                        }
-                    } catch (e: RoomNotThereException) {
-                        view.content.output.respond(e.message)
-                    }
-                } else {
-                    view.content.output.respond("this direction doesn't exist")
-                }
-            }
-        } else {
+        if (splitInput[0] !in commands.keys) {
             view.content.output.respond("this command doesn't exist")
         }
 
+        when (splitInput[0]) {
+            "move" -> {
+                if (splitInput[1] !in commands[splitInput[0]]!!.keys) {
+                    view.content.output.respond("this direction doesn't exist")
+                    return
+                }
 
+                try {
+                    model.map.move(commands["move"]!![splitInput[1]] as Int, model.hero)
+                    updateMap()
+                    view.content.output.respond("moved to the ${splitInput[1]}")
+
+                    if(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.filterIsInstance<Enemy>().isNotEmpty()){
+                        for (i in 0 ..< model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.size){
+                            view.content.output.respond(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities[i].name)
+                        }
+                        combat(model.hero, model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.entities.filterIsInstance<Enemy>()[0])
+                    }
+                    for (i in 0 ..< model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.inventory.content.size){
+                        view.content.output.respond(model.map.map[model.hero.room.coords.x][model.hero.room.coords.y]!!.inventory.content[i].name)
+                    }
+                } catch (e: RoomNotThereException) {
+                    view.content.output.respond(e.message)
+                }
+            }
+
+            "inventory" -> {
+                if (splitInput[1] !in commands[splitInput[0]]!!.keys) {
+                    view.content.output.respond("this command doesn't exist")
+                    return
+                }
+
+                when (splitInput[1]) {
+                    "drop" -> {
+                        try {
+                            model.hero.dropItem(model.hero.inventory.content[splitInput[2].toInt()])
+                        } catch (e: java.lang.IndexOutOfBoundsException) {
+                            view.content.output.respond("item id ${splitInput[2]} doesn't correspond to an item in your inventory")
+                        } catch (e: ItemNotThereException) {
+                            view.content.output.respond(e.message)
+                        }
+                    }
+
+                    "pickup" -> {
+                        try {
+                            model.hero.pickupItem(model.hero.room.inventory.content[splitInput[2].toInt()])
+                        } catch (e: IndexOutOfBoundsException) {
+                            view.content.output.respond("this item id doesn't correspond to an item in the current room")
+                        } catch (e: ItemNotThereException) {
+                            view.content.output.respond(e.message)
+                        }
+                    }
+                }
+            }
+
+            else -> {
+                view.content.output.respond("this command doesn't exist!")
+                return
+            }
+        }
     }
     fun combat(hero: Hero, defender: Entity){
         return
