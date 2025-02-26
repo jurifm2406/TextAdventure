3adf0ddb70cd9fe5032429a7deaf4a352d4bb648
Franz Münzner <jurifm@icloud.com>
2025-02-26 11:55:11 +0100

    cleaned up enemy class

--- src/model/objects/base/entities/Enemy.kt
+++ src/model/objects/base/entities/Enemy.kt
@@ -6,11 +6,12 @@ import model.objects.base.item.Weapon
 import model.objects.world.Room
 import java.awt.Point
 
-class Enemy(name: String = "Olaf",
-            health: Int = 10,
-            inventory: Inventory = Inventory(5),
-            weapon: Weapon = Weapon("fists", "mighty fists", 2),
-            armor: Armor = Armor("nothing", "bare skin", 0, 1.0),
-            room: Room = Room(Point(-1,-1))) :
-    Entity(name, health, inventory, weapon, armor, room) {
-}
+class Enemy(
+    name: String = "",
+    health: Int = 10,
+    inventory: Inventory = Inventory(5),
+    weapon: Weapon = Weapon("fists", "mighty fists", 2),
+    armor: Armor = Armor("nothing", "bare skin", 0, 1.0),
+    room: Room = Room(Point(-1, -1))
+) :
+    Entity(name, health, inventory, weapon, armor, room)
