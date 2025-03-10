d9cb97b6a5fc45ed04706588e75ae409643ef4b7
Tempestes <max@fissguss.de>
2025-03-10 21:47:35 +0100

    added comments to model

--- src/model/Model.kt
+++ src/model/Model.kt
@@ -3,7 +3,14 @@ package model
 import model.objects.base.entities.Hero
 import java.awt.Point
 import javax.swing.table.DefaultTableModel
-
+/**
+ * The main game model that manages the hero, map, and game state.
+ *
+ * This class initializes the game world, including the hero and the map.
+ * It also tracks the current floor level and provides an information model for the UI.
+ *
+ * @param heroName The name of the hero character controlled by the player.
+ */
 class Model(heroName: String) {
     val mapSize = Point(9, 9)
     var map = Map(mapSize, 0)
