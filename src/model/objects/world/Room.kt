4bf1f2d476e9f7e6a1fc6a2694452f1a0e4c16ce
Tempestes <max@fissguss.de>
2025-03-10 21:47:42 +0100

    added comments to Room

--- src/model/objects/world/Room.kt
+++ src/model/objects/world/Room.kt
@@ -3,7 +3,14 @@ package model.objects.world
 import model.objects.base.Inventory
 import model.objects.base.entities.Entity
 import java.awt.Point
-
+/**
+ * Represents a room in the game world.
+ *
+ * Each room has a set of coordinates, an inventory to store items,
+ * and a list of entities (such as players or enemies) that may occupy the room.
+ *
+ * @param coords The position of the room within the game world.
+ */
 class Room(val coords: Point) {
     val inventory = Inventory(64)
     val entities = mutableListOf<Entity>()
