0a58944ac003fa8b0f2d3f2ac99e3aa35ee301fb
Tempestes <max@fissguss.de>
2025-03-10 21:47:27 +0100

    added comments to item

--- src/model/objects/base/item/Item.kt
+++ src/model/objects/base/item/Item.kt
@@ -1,3 +1,11 @@
 package model.objects.base.item
-
+/**
+ * The Item class serves as an abstract base for all items in the game.
+ *
+ * It encapsulates common properties such as the item's name and a brief description.
+ * Specific types of items (e.g., weapons, armors, consumables) will extend this class.
+ *
+ * @param name The name of the item.
+ * @param description A brief description of the item.
+ */
 abstract class Item(val name: String, val description: String)
